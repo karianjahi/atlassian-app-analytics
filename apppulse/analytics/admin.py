0b945aad7c05a72becbe8099247b8ca3aabadebb
karianjahi <joseph@spiced-academy.com>
2026-04-27 10:37:25 +0200

    Register the hcustomer health model with admin

--- apppulse/analytics/admin.py
+++ apppulse/analytics/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Customer, UsageEvent
+from .models import Customer, UsageEvent, CustomerHealth
 
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(UsageEvent)
+admin.site.register(CustomerHealth)
