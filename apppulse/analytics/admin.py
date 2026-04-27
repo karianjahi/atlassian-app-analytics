7ff3b86abb3c24abeef6d96e30767faa8775af36
karianjahi <joseph@spiced-academy.com>
2026-04-27 10:24:57 +0200

    Register the model with admin make migrations

--- apppulse/analytics/admin.py
+++ apppulse/analytics/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, UsageEvent
 
 # Register your models here.
 admin.site.register(Customer)
+admin.site.register(UsageEvent)
