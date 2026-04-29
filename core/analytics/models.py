59b7a89fb175ee98269203efda9635b2ce91963c
karianjahi <joseph@spiced-academy.com>
2026-04-29 09:21:47 +0200

    Add a customer model

---
+++ core/analytics/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    company_name = models.CharField(max_length=255)
+    country = models.CharField(max_length=100)
+    company_size = models.PositiveBigIntegerField()
+    
+    LICENSE_CHOICES = [
+        ("free", "Free"),
+        ("standard", "Standard"),
+        ("premium", "Premium")
+    ]
+    
+    license_tier = models.CharField(max_length=20, choices=LICENSE_CHOICES, default="free")
+    app_name = models.CharField(max_length=255)
+    installed_at = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.company_name (self.app_name)}"
+    
