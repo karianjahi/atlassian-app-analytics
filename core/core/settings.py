b168cc811df938d4cf87801745d5359c1290363f
karianjahi <joseph@spiced-academy.com>
2026-04-29 10:13:48 +0200

    Configure postgresql database under settings

--- core/core/settings.py
+++ core/core/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'atlassian',
+        'USER': 'karianjahi',
+        'PASSWORD': 'JahyeT7?Tem',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
