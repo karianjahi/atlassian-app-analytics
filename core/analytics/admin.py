328f8cc93ca552507f02634649986e4241d8e606
karianjahi <joseph@spiced-academy.com>
2026-04-29 12:26:52 +0200

    Add customer health model to the apps admin panel to view in the UI

--- core/analytics/admin.py
+++ core/analytics/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Customer, UsageEvent
+from .models import Customer, UsageEvent, CustomerHealth
 
 admin.site.register(Customer)
 admin.site.register(UsageEvent)
+admin.site.register(CustomerHealth)
