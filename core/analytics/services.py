b37a0019c867ff594532382c35cb8c5630ce6d1a
karianjahi <joseph@spiced-academy.com>
2026-04-30 11:49:24 +0200

    Calculate and update the database on the various scores as well as the default values for the customer health model

--- core/analytics/services.py
+++ core/analytics/services.py
@@ -29,6 +29,55 @@ def calculate_customer_health(customer: Customer):
     error_count = recent_events.filter(event_type="sync_failed").count()
     support_count = recent_events.filter(event_type="opened_support_ticket").count()
     
+    # Lets get the unique features used based on usage events for this customer
+    unique_features_used = (
+        recent_events
+        .filter(event_type__in=FEATURE_EVENTS)
+        .values("event_type")
+        .order_by() # this removes ordering before calling distinct
+        .distinct()
+        .count()
+        )
+    
+    # Get the scores
+    usage_score = min(total_recent_events * 5, 100) # usage is no of events x 5 or 100 whichever is smaller
+    feature_adoption_score = (unique_features_used/len(FEATURE_EVENTS)) * 100
+    reliability_score = max(100 - error_count * 15, 0) # Penalizes fails
+    support_score = max(100 - support_count * 20, 0) # if no support, then perfect. if support, magnify the problem
+    
+    # calculate health score for this customer
+    health_score = (
+        ALPHA * usage_score
+        + BETA * feature_adoption_score
+        + GAMMA * reliability_score
+        + DELTA * support_score
+    )
+    
+    # Determine the risk label
+    if health_score >= 80:
+        risk_label = "healthy"
+    elif health_score >= 50:
+        risk_label = "watch"
+    else:
+        risk_label = "high_risk"
+        
+    # What is the churn risk for this customer
+    churn_risk = 100 - health_score # higher churn risk for low customer health
+    
+    # Update the customer health model (table)
+    customer_health, created = CustomerHealth.objects.update_or_create(
+        customer=customer,
+        defaults={
+            "usage_score": round(usage_score, 2),
+            "feature_adoption_score": round(feature_adoption_score, 2),
+            "reliability_score": round(reliability_score, 2),
+            "support_score": round(support_score, 2),
+            "health_score": round(health_score, 2),
+            "churn_risk": round(churn_risk, 2),
+            "risk_label": risk_label,
+        }
+    )
+    return customer_health
     
     
     
