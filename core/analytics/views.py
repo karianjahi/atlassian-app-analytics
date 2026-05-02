854a8cab0deefb03e4e5c1fdb43e08a6ad2aa8c5
karianjahi <joseph@spiced-academy.com>
2026-05-02 11:56:34 +0200

    Create views for customer health

--- core/analytics/views.py
+++ core/analytics/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Customer, CustomerHealth
+from analytics.services import calculate_customer_health_for_all_customers
 
-# Create your views here.
+def dashboard(request):
+    calculate_customer_health_for_all_customers()
+    
+    total_customers = Customer.objects.count()
+    health_records = CustomerHealth.objects.select_related("customer")
+    
+    healthy_count = health_records.filter(risk_label="healthy").count()
+    watch_count = health_records.filter(risk_label="watch").count()
+    high_risk_count = health_records.filter(risk_label="high_risk").count()
+    
+    average_health_score = 0
+    if total_customers > 0:
+        total_health_score = sum([record.health_score for record in health_records])
+        average_health_score = total_health_score/len(health_records)
+    
+    context = {
+        "total_customers": total_customers,
+        "healthy_count": healthy_count,
+        "watch_count": watch_count,
+        "high_risk_count": high_risk_count,
+        "average_health_score": round(average_health_score, 2),
+        "health_records": health_records
+    }
+    
+    return render(request, "analytics/dashboard.html", context)
+    
+    
