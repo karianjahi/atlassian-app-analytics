fa772070d5fbe4aa9601d971107aeb978ef128f5
karianjahi <joseph@spiced-academy.com>
2026-05-04 19:29:52 +0200

    Fix missing comma in the urlpatterns list of the analytics app

--- core/analytics/urls.py
+++ core/analytics/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard")
+    path("", views.dashboard, name="dashboard"),
     path("customers/<int:customer_id>", views.customer_detail, name="customer_detail")
 ]
 
