2d0834b736e306a7be2ad7adb036e8eefbff6812
karianjahi <joseph@spiced-academy.com>
2026-05-05 18:36:23 +0200

    Refactor analytics.urls to rid of a bug brought about by an additional comma

--- core/analytics/urls.py
+++ core/analytics/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns = [
     path("", views.landing_page, name="landing_page"),
-    path("dashboard/", views.dashboard, name="dashboard"),,
+    path("dashboard/", views.dashboard, name="dashboard"),
     path("customers/<int:customer_id>/", views.customer_detail, name="customer_detail")
 ]
 
