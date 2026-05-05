66dd9a02fd0e4befef1b34c9f4695b7f387fd129
karianjahi <joseph@spiced-academy.com>
2026-05-05 11:08:28 +0200

    Remove chart if replotting required

--- core/analytics/static/analytics/js/customer_detail.js
+++ core/analytics/static/analytics/js/customer_detail.js
@@ -1,9 +1,54 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-    const eventLabels = JSON.parser(document.getElementById("event-labels").textContent);
+let eventChartInstance = null;
+let timeChartInstance = null;
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const eventLabels = JSON.parse(document.getElementById("event-labels").textContent);
     const eventCounts = JSON.parse(document.getElementById("event-counts").textContent);
     const timeLabels = JSON.parse(document.getElementById("time-labels").textContent);
     const timeCounts = JSON.parse(document.getElementById("time-counts").textContent);
 
+    const eventCanvas = document.getElementById("eventChart");
 
+    if (eventCanvas) {
+        if (eventChartInstance) {
+            eventChartInstance.destroy(); // otherwise it plots on top in a loop
+        }
+        eventChartInstance = new Chart(eventCanvas, {
+            type: "bar",
+            data: {
+                labels: eventLabels,
+                datasets: [{
+                    label: "Number of Events",
+                    data: eventCounts
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        });
+    }
+    const timeCanvas = document.getElementById("timeChart");
+
+    if (timeCanvas) {
+        if (timeChartInstance) {
+            timeChartInstance.destroy();
+        }
+        timeChartInstance = new Chart(timeCanvas, {
+            type: "line",
+            data: {
+                labels: timeLabels,
+                datasets: [{
+                    label: "Events Over Time",
+                    data: timeCounts,
+                    tension: 0.3
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        })
+    }
 });
