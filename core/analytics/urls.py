673086b5fab5cb824fde2a3fb7cfd6997c68ea70
karianjahi <joseph@spiced-academy.com>
2026-05-05 18:18:06 +0200

    Add a landing page url

--- core/analytics/urls.py
+++ core/analytics/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard"),
+    path("", views.landing_page, name="landing_page"),
+    path("dashboard/", views.dashboard, name="dashboard"),,
     path("customers/<int:customer_id>/", views.customer_detail, name="customer_detail")
 ]
 
