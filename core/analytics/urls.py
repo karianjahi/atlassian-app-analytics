1440f75dcff6ded8818934377a0146f23e5c11a3
karianjahi <joseph@spiced-academy.com>
2026-05-06 17:04:41 +0200

    Add url for customers api

--- core/analytics/urls.py
+++ core/analytics/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path("", views.landing_page, name="landing_page"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("customers/<int:customer_id>/", views.customer_detail, name="customer_detail")
+    
+    ## api urls
+    path("api/customers/", views.customer_list_api),
 ]
 
