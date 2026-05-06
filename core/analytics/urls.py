27350e5ff98f89c029193b38472237a07162f6e6
karianjahi <joseph@spiced-academy.com>
2026-05-06 17:09:59 +0200

    Correct an omitted comma bug from analytics.urls

--- core/analytics/urls.py
+++ core/analytics/urls.py
@@ -4,7 +4,7 @@ from .import views
 urlpatterns = [
     path("", views.landing_page, name="landing_page"),
     path("dashboard/", views.dashboard, name="dashboard"),
-    path("customers/<int:customer_id>/", views.customer_detail, name="customer_detail")
+    path("customers/<int:customer_id>/", views.customer_detail, name="customer_detail"),
     
     ## api urls
     path("api/customers/", views.customer_list_api),
