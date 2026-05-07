c796b951cd4ed3f492bd71996e54743461ec2a74
karianjahi <joseph@spiced-academy.com>
2026-05-07 05:10:32 +0200

    Make dashboard view function API-driven

--- core/analytics/views.py
+++ core/analytics/views.py
@@ -15,31 +15,9 @@ from .serializers import CustomerSerializer, CustomerHealthSerializer, UsageEven
 
 def landing_page(request):
     return render(request, "analytics/landing_page.html")
+
 def dashboard(request):
-    
-    total_customers = Customer.objects.count()
-    health_records = CustomerHealth.objects.select_related("customer")
-    
-    healthy_count = health_records.filter(risk_label="healthy").count()
-    watch_count = health_records.filter(risk_label="watch").count()
-    high_risk_count = health_records.filter(risk_label="high_risk").count()
-    
-    average_health_score = 0
-    if total_customers > 0:
-        total_health_score = sum([record.health_score for record in health_records])
-        average_health_score = total_health_score/len(health_records)
-    
-    context = {
-        "total_customers": total_customers,
-        "healthy_count": healthy_count,
-        "watch_count": watch_count,
-        "high_risk_count": high_risk_count,
-        "average_health_score": round(average_health_score, 2),
-        "health_records": health_records
-    }
-    
-    return render(request, "analytics/dashboard.html", context)
-    
+    return render(request, "analytics/dashboard.html")
 
 def customer_detail(request, customer_id):
     customer = get_object_or_404(Customer, id=customer_id)
