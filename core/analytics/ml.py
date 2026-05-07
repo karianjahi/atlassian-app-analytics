156096aacaba78b8c4f9c1e12afccd70b7c750a6
karianjahi <joseph@spiced-academy.com>
2026-05-07 18:26:37 +0200

    Update train churn model to include model evaluation where we train-test-split to determine various scores e.g. accuracy

--- core/analytics/ml.py
+++ core/analytics/ml.py
@@ -44,9 +44,25 @@ def train_churn_model():
         ]
     ]
     y = df["did_churn"]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42,
+    )
+
     model = LogisticRegression(max_iter=1000)
-    model.fit(X, y)
-    return model
+
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, predictions)
+
+    return {
+        "model": model,
+        "accuracy": round(accuracy * 100, 2),
+    }
 
 
 def predict_churn_probability(customer_health:CustomerHealth):
