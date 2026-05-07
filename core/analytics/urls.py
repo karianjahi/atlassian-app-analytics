c34eb230c8250facce3958bab122914d410041bc
karianjahi <joseph@spiced-academy.com>
2026-05-07 19:00:59 +0200

    Add corresponding url for feature importance

--- core/analytics/urls.py
+++ core/analytics/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path("api/summary", views.summary_api),
     path("api/dashboard/", views.dashboard_data_api),
     path("api/customers/<int:customer_id>/detail/", views.customer_detail_data_api, name="customer_detail_data_api"),
-    path("api/ml/metrics/", views.ml_model_metrics_api)
+    path("api/ml/metrics/", views.ml_model_metrics_api),
+    path("api/ml/feature-importance", views.ml_feature_importance_api)
 ]
 
