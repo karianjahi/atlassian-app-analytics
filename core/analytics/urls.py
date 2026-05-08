205704c7c81282d27745a255649863d89de304fe
karianjahi <joseph@spiced-academy.com>
2026-05-08 07:34:39 +0200

    Create a URL for customer health snapshots API endpoint

--- core/analytics/urls.py
+++ core/analytics/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path("api/dashboard/", views.dashboard_data_api),
     path("api/customers/<int:customer_id>/detail/", views.customer_detail_data_api, name="customer_detail_data_api"),
     path("api/ml/metrics/", views.ml_model_metrics_api),
-    path("api/ml/feature-importance/", views.ml_feature_importance_api)
+    path("api/ml/feature-importance/", views.ml_feature_importance_api),
+    path("api/customers/<int:customer_id>/health-history", views.customer_health_history_api)
 ]
 
