0bfb4bc8059c3ddee546ff290163254b32384c85
karianjahi <joseph@spiced-academy.com>
2026-05-09 10:03:43 +0200

    Fix a bug on csvuploadform

--- core/analytics/forms.py
+++ core/analytics/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 
 class CSVUploadForm(forms.Form):
-    csv_file = forms.FileField(label="CSV file", error_messages="empty": "Either the file is empty or invalid!")
+    csv_file = forms.FileField(
+        label="CSV file", 
+        error_messages = {
+            "empty": "Either the file is empty or invalid!"
+        }
+        )
     
     
