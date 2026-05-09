ee51e25c63198cfe5313e2a15e6b942ec7ad0c57
karianjahi <joseph@spiced-academy.com>
2026-05-09 10:02:41 +0200

    Customize error message from Dango forms when an invalid form is submitted

--- core/analytics/forms.py
+++ core/analytics/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 class CSVUploadForm(forms.Form):
-    csv_file = forms.FileField(label="CSV file")
+    csv_file = forms.FileField(label="CSV file", error_messages="empty": "Either the file is empty or invalid!")
+    
     
