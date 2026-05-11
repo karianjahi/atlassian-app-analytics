48a2d8d2a16e5ec3f90b97395c2e65562331d9a8
karianjahi <joseph@spiced-academy.com>
2026-05-11 19:10:38 +0200

    Add csrf verification for post requests

--- core/core/settings.py
+++ core/core/settings.py
@@ -34,6 +34,17 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in os.environ.get("ALLOWED_HOSTS", "*").split(",")
+    if host.strip()
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip()
+    for origin in os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
+    if origin.strip()
+]
 
 # Application definition
 
